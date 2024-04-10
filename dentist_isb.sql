9371a1b801d006a6b2b2163f551e039a68299dd5
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-10 23:15:52 +0100

    workin away

--- dentist_isb.sql
+++ dentist_isb.sql
@@ -19,29 +19,34 @@ INSERT INTO `patient` (`name`, `gender`, `date_of_birth`, `address`, `email`, `p
 ('Michael Shannon', 'M', '1974-08-07', '789 Oak St, Chicago', 'mike@hotmail.com', '1357924680'),
 ('Ayo Edebiri', 'F', '1997-08-27', '321 Pine St, New York', 'ayo@protonmail.com', '2468135790'),
 ('David Lynch', 'M', '1946-01-20', '654 Cedar St, Los Angeles', 'david@davidlynch.com', '9876543210'),
-('Greta Gerwig', 'F', '1983-08-04', '987 Birch St, New York', 'greta@yahoo.com', '3692581470');
+('Greta Gerwig', 'F', '1983-08-04', '987 Birch St, New York', 'greta@yahoo.com', '3692581470'), 
+('Stephen Rea', 'M', '1946-10-31', '147 Maple St, Dublin', 'stephen@yahoo.ie', '2581473690');
 
 
 
 CREATE TABLE `staff` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `license_number` VARBINARY(255) NOT NULL,
     `name` VARCHAR(255),
     `age` TINYINT UNSIGNED NOT NULL,
     `gender` ENUM ('M', 'F'),
     `position` ENUM ('Dentist', 'Hygienist', 'Receptionist', 'Assistant')
-);
-INSERT INTO staff (name, age, gender, position) VALUES
-('Dr. Brown', 45, 'M', 'Dentist'),
-('Dr. Green', 40, 'F', 'Dentist'),
-('Ms. White', 30, 'F', 'Hygienist'),
-('Mr. Black', 35, 'M', 'Receptionist'),
-('Ms. Blue', 25, 'F', 'Hygienist'),
-('Mr. Red', 50, 'M', 'Assistant'),
-('Ms. Yellow', 28, 'F', 'Assistant');
+) AUTO_INCREMENT = 2000;
+INSERT INTO `staff` (`name`, `license_number`, `age`, `gender`, `position`) VALUES 
+('Dr. John Doe', AES_ENCRYPT('123456', 'password'), 35, 'M', 'Dentist'),
+('Dr. Jane Smith', AES_ENCRYPT('654321', 'password'), 40, 'F', 'Dentist'),
+('Dr. Michael Johnson', AES_ENCRYPT('456789', 'password'), 45, 'M', 'Dentist'),
+('Dr. Sarah Williams', AES_ENCRYPT('987654', 'password'), 30, 'F', 'Dentist'),
+('Dr. David Brown', AES_ENCRYPT('789456', 'password'), 50, 'M', 'Dentist'),
+('Dr. Emily Davis', AES_ENCRYPT('321654', 'password'), 25, 'F', 'Dentist'),
+('Dr. Robert Wilson', AES_ENCRYPT('654987', 'password'), 55, 'M', 'Dentist'),
+('Dr. Laura Martinez', AES_ENCRYPT('987321', 'password'), 28, 'F', 'Dentist'),
+('Dr. James Taylor', AES_ENCRYPT('456123', 'password'), 60, 'M', 'Dentist'),
+('Dr. Maria Garcia', AES_ENCRYPT('123789', 'password'), 33, 'F', 'Dentist');
 
 
 
-CREATE TABLE dental_report (
+CREATE TABLE `dental_report` (
     `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `patient_id` INT UNSIGNED NOT NULL,
     `staff_id` INT UNSIGNED NOT NULL,
@@ -49,16 +54,16 @@ CREATE TABLE dental_report (
     `diagnosis` VARCHAR(255),
     `treatment` VARCHAR(255),
     `xray_image` MEDIUMBLOB,
-    FOREIGN KEY (patient_id) REFERENCES patient(id),
-    FOREIGN KEY (staff_id) REFERENCES staff(id)
-) AUTO_INCREMENT = 2000;
-INSERT INTO dental_report (patient_id, staff_id, datetime, diagnosis, treatment, xray_image) VALUES
-(1000, 1, '2022-01-01 09:30:00', 'Cavity', 'Filling', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray1.jpeg')),
-(1001, 2, '2022-01-02 10:30:00', 'Gum disease', 'Deep cleaning', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray2.jpeg')),
-(1002, 3, '2022-01-03 11:30:00', 'Tooth decay', 'Extraction', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray3.jpeg')),
-(1003, 4, '2022-01-04 12:30:00', 'Toothache', 'Root canal', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray4.jpeg')),
-(1004, 5, '2022-01-05 13:30:00', 'Cracked tooth', 'Crown', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray5.jpeg')),
-(1005, 6, '2022-01-06 14:30:00', 'Abscess', 'Drainage', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray6.jpeg'));
+    FOREIGN KEY (`patient_id`) REFERENCES `patient`(`id`),
+    FOREIGN KEY (`staff_id`) REFERENCES `staff`(`id`)
+) AUTO_INCREMENT = 3000;
+INSERT INTO `dental_report` (`patient_id`, `staff_id`, `datetime`, `diagnosis`, `treatment`, `xray_image`) VALUES
+(1000, 2000, '2022-01-01 09:30:00', 'Cavity', 'Filling', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray1.jpeg')),
+(1001, 2001, '2022-01-02 10:30:00', 'Gum disease', 'Deep cleaning', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray2.jpeg')),
+(1002, 2002, '2022-01-03 11:30:00', 'Tooth decay', 'Extraction', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray3.jpeg')),
+(1003, 2003, '2022-01-04 12:30:00', 'Toothache', 'Root canal', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray4.jpeg')),
+(1004, 2004, '2022-01-05 13:30:00', 'Cracked tooth', 'Crown', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray5.jpeg')),
+(1005, 2005, '2022-01-06 14:30:00', 'Abscess', 'Drainage', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/dentist-isb/xray/xray6.jpeg'));
 
 
 
@@ -70,23 +75,23 @@ CREATE TABLE `appointment` (
     `datetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `duration` INT DEFAULT 60,
     `reason` VARCHAR(255),
-    FOREIGN KEY (patient_id) REFERENCES patient(id),
-    FOREIGN KEY (staff_id) REFERENCES staff(id)
+    FOREIGN KEY (`patient_id`) REFERENCES `patient`(`id`),
+    FOREIGN KEY (`staff_id`) REFERENCES `staff`(`id`)
 );
-INSERT INTO appointment (booking_type, patient_id, staff_id, datetime, duration, reason) VALUES 
-('Phone', 100, 1, '2022-01-01 09:00:00', 30, 'Check-up'),
-('In-Person', 101, 2, '2022-01-02 10:00:00', 60, 'Cleaning'),
-('Online', 102, 3, '2022-01-03 11:00:00', 40, 'Extraction'),
-('Postal', 103, 4, '2022-01-04 12:00:00', 30, 'Consultation'),
-('Phone', 104, 5, '2022-01-05 13:00:00', 60, 'Filling'),
-('In-Person', 105, 6, '2022-01-06 14:00:00', 60, 'Root Canal'),
-('Online', 106, 7, '2022-01-07 15:00:00', 30, 'X-ray');
+INSERT INTO `appointment` (`booking_type`, `patient_id`, `staff_id`, `datetime`, `duration`, `reason`) VALUES 
+('Phone', 1000, 2000, '2022-01-01 09:00:00', 30, 'Check-up'),
+('In-Person', 1001, 2001, '2022-01-02 10:00:00', 60, 'Cleaning'),
+('Online', 1002, 2002, '2022-01-03 11:00:00', 40, 'Extraction'),
+('Postal', 1003, 2003, '2022-01-04 12:00:00', 30, 'Consultation'),
+('Phone', 1004, 2004, '2022-01-05 13:00:00', 60, 'Filling'),
+('In-Person', 1005, 2005, '2022-01-06 14:00:00', 60, 'Root Canal'),
+('Online', 1006, 2006, '2022-01-07 15:00:00', 30, 'X-ray');
 
-CREATE TABLE billing (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    patient_id INT UNSIGNED NOT NULL,
-    date DATE,
-    amount DECIMAL(10, 2),
-    FOREIGN KEY (patient_id) REFERENCES patient(id)
+CREATE TABLE `billing` (
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `patient_id` INT UNSIGNED NOT NULL,
+    `date` DATE,
+    `amount` DECIMAL(10, 2),
+    FOREIGN KEY (`patient_id`) REFERENCES `patient`(`id`)
 );
 
